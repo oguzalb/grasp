7376fe9d6ee38c1211c6d92a84335b8e5533fed0
Oguz Albayrak <oguzalba@gmail.com>
2016-03-13 21:33:03 -0700

    added string concatenation

--- types/int.cpp
+++ types/int.cpp
@@ -10,7 +10,7 @@ Int::Int(int ival) {
     this->ival = ival;
 }
 
-void equals() {
+void int_equals() {
     Int *o1 = POP_TYPE(Int, int_type);
     Int *o2 = POP_TYPE(Int, int_type);
     if (o1->ival == o2->ival) {
@@ -20,28 +20,28 @@ void equals() {
     }
 }
 
-void add() {
+void int_add() {
     Int *o1 = POP_TYPE(Int, int_type);
     Int *o2 = POP_TYPE(Int, int_type);
     // TODO exc
     PUSH(new Int(o1->ival + o2->ival));
 }
 
-void sub() {
+void int_sub() {
     Int *o1 = POP_TYPE(Int, int_type);
     Int *o2 = POP_TYPE(Int, int_type);
     // TODO exc
     PUSH(new Int(o2->ival - o1->ival));
 }
  
-void mul() {
+void int_mul() {
     Int *o1 = POP_TYPE(Int, int_type);
     Int *o2 = POP_TYPE(Int, int_type);
     // TODO exc
     PUSH(new Int(o1->ival * o2->ival));
 }
  
-void div() {
+void int_div() {
     Int *o1 = POP_TYPE(Int, int_type);
     Int *o2 = POP_TYPE(Int, int_type);
     // TODO exc
@@ -50,9 +50,9 @@ void div() {
 
 void init_int() {
     int_type = new Class("int", NULL);
-    int_type->setmethod("__add__", add);
-    int_type->setmethod("__sub__", sub);
-    int_type->setmethod("__mul__", mul);
-    int_type->setmethod("__div__", div);
-    int_type->setmethod("__equals__", equals);
+    int_type->setmethod("__add__", int_add);
+    int_type->setmethod("__sub__", int_sub);
+    int_type->setmethod("__mul__", int_mul);
+    int_type->setmethod("__div__", int_div);
+    int_type->setmethod("__equals__", int_equals);
 }
--- types/string.cpp
+++ types/string.cpp
@@ -8,6 +8,14 @@ String::String(string sval) {
     this->sval = sval;
 }
 
+void str_add() {
+    String *other = POP_TYPE(String, str_type);
+    String *self = POP_TYPE(String, str_type);
+    // TODO exc
+    PUSH(new String(self->sval + other->sval));
+}
+
 void init_string() {
     str_type = new Class("str", NULL);
+    str_type->setmethod("__add__", str_add);
 }
